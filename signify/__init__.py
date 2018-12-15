058e19f07ecffac77c3e9acbf3563ee26da89569
Ralph Broenink <ralph@ralphbroenink.net>
2018-12-15 15:52:50 +0100

    Fix error where hashlib functions would be detected improperly

--- signify/__init__.py
+++ signify/__init__.py
@@ -6,9 +6,9 @@ def _print_type(t):
         return ""
     elif isinstance(t, tuple):
         return ".".join(map(str, t))
-    elif hasattr(t, "__name__"):
-        return t.__name__
     elif callable(t) and hasattr(t(), 'name'):
         return t().name  # used by hashlib
+    elif hasattr(t, "__name__"):
+        return t.__name__
     else:
         return type(t).__name__
